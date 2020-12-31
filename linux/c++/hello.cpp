a7e26ffcd4fa7ec5a17be819a0d223f75f115fee
Qingbin Wei <lenlenshao@gmail.com>
2021-01-01 03:27:32 +0800

    linux

---
+++ linux/c++/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, world!" << endl;
+    string s;
+    cin >> s;
+    cout << s << endl;
+    return 0;
+}
